ffaf9d13cab886facffd04004726d6b045af3b6b
miskhill <miskhill@hotmail.com>
2022-05-07 21:31:55 +0100

    Added html css and js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.bg').hover(function() {
+      alert('You Died!')
+    })
+    
+    $('.finish').hover(function() {
+      alert('Congratulations, you win!')
+    })
+    
+  }) 
